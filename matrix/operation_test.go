772d9fcd4f4494a953666f79ca25babfa300a1d9
e.birukov <jhnsft2010@gmail.com>
2021-08-23 10:59:27 +0300

    add miss test

--- matrix/operation_test.go
+++ matrix/operation_test.go
@@ -8,6 +8,14 @@ import (
 )
 
 func TestMultiple(t *testing.T) {
+	testMultiple(t, Multiple)
+}
+
+func TestMultipleV(t *testing.T) {
+	testMultiple(t, MultipleV)
+}
+
+func testMultiple(t *testing.T, m func(ma, mb *Matrix) *Matrix) {
 	type args struct {
 		ma *Matrix
 		mb *Matrix
@@ -41,38 +49,75 @@ func TestMultiple(t *testing.T) {
 			},
 			NewMatrix(4, 3, 140, 125, 110, 95, 440, 400, 360, 320, 740, 675, 610, 545),
 		},
+		{
+			"4x4*4x4Matrix",
+			args{
+				ma: NewMatrix(4, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+				mb: NewMatrix(4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+			},
+			NewMatrix(4, 4, 10, 10, 10, 10, 26, 26, 26, 26, 42, 42, 42, 42, 58, 58, 58, 58),
+		},
+		{
+			"8x8*8x8Matrix",
+			args{
+				ma: NewMatrix(8, 8, constMatrixValues(8*8, 2.0)...),
+				mb: NewMatrix(8, 8, constMatrixValues(8*8, 1.0)...),
+			},
+			NewMatrix(8, 8, constMatrixValues(8*8, 16.0)...),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Multiple(tt.args.ma, tt.args.mb); !reflect.DeepEqual(got, tt.want) {
+			if got := m(tt.args.ma, tt.args.mb); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Multiple() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func BenchmarkMultiple1000x1000(b *testing.B) {
-	benchmarkMultiple(b, 1000, 1000)
+func BenchmarkMultipleVector1000x1000(b *testing.B) {
+	benchmarkMultiple(b, 1000, 1000, MultipleV)
 }
 
-var res *Matrix
+func BenchmarkMultipleAVX2_8x8(b *testing.B) {
+	benchmarkMultiple(b, 8, 8, MultipleAVX)
+}
 
-func createMatrixValues(n int) []int {
+func BenchmarkMultiple_8x8(b *testing.B) {
+	benchmarkMultiple(b, 8, 8, Multiple)
+}
+
+func BenchmarkMultipleAVX2_1000x1000(b *testing.B) {
+	benchmarkMultiple(b, 1000, 1000, MultipleAVX)
+}
+
+func BenchmarkMultiple1000x1000(b *testing.B) {
+	benchmarkMultiple(b, 1000, 1000, Multiple)
+}
+
+func constMatrixValues(n int, value float32) []float32 {
 	rand.Seed(time.Now().Unix())
-	values := make([]int, n)
+	values := make([]float32, n)
 	for i := range values {
-		values[i] = rand.Int()
+		values[i] = value
 	}
 	return values
 }
 
-func benchmarkMultiple(b *testing.B, length, height int) {
+func createMatrixValues(n int) []float32 {
+	rand.Seed(time.Now().Unix())
+	values := make([]float32, n)
+	for i := range values {
+		values[i] = rand.Float32()
+	}
+	return values
+}
+
+func benchmarkMultiple(b *testing.B, length, height int, method func(ma, mb *Matrix) *Matrix) {
 	ma := NewMatrix(length, height, createMatrixValues(length*height)...)
 	mb := NewMatrix(length, height, createMatrixValues(height*length)...)
 	b.ResetTimer()
-	var mc *Matrix
 	for i := 0; i < b.N; i++ {
-		mc = Multiple(ma, mb)
+		method(ma, mb)
 	}
-	res = mc
 }
